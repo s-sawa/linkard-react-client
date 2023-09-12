e0863bc9ec158fc3133051cb77c3cee7554d98a5
s-sawa <s.s.hrmeee21@gmail.com>
2023-09-12 13:27:57 +0900

    入力フォームのその他項目名変更対応

---
+++ src/components/QR/QRCodeModal.jsx
@@ -0,0 +1,33 @@
+// QRCodeModal.js
+
+import React from "react";
+import Modal from "react-modal";
+import QRCode from "qrcode.react"; // QRコード生成ライブラリ
+
+const customStyles = {
+  content: {
+    width: "50%",
+    height: "50%",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+const QRCodeModal = ({ isOpen, onRequestClose, url }) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      style={customStyles}
+      contentLabel="QR Code Modal"
+    >
+      <div>
+        <h2>Your Profile QR Code</h2>
+        <QRCode value={url} size={256} />
+      </div>
+    </Modal>
+  );
+};
+
+export default QRCodeModal;
