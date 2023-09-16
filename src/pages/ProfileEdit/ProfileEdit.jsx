4872219f96a6e4d4c8e7f8d682a026ac4b9ed1df
s-sawa <s.s.hrmeee21@gmail.com>
2023-09-16 11:30:03 +0900

    プロフィールedit画像対応済

--- src/pages/ProfileEdit/ProfileEdit.jsx
+++ src/pages/ProfileEdit/ProfileEdit.jsx
@@ -1,18 +1,18 @@
 import axios from "axios";
 import Cookies from "js-cookie";
-import { useForm, useFieldArray } from "react-hook-form";
 import { useEffect, useState } from "react";
+import { useForm, useFieldArray } from "react-hook-form";
 
 const ProfileEdit = () => {
   const [profileData, setProfileData] = useState(null);
+  const token = Cookies.get("token") || null;
 
-  const { register, handleSubmit, getValues, setValue, errors, control } =
-    useForm({});
+  const { register, handleSubmit, getValues, setValue, control } = useForm({});
 
   const {
     fields,
     append: appendHobbies,
-    remove: removeHobbies,
+    remove,
   } = useFieldArray({
     control,
     name: "hobbies",
@@ -20,89 +20,103 @@ const ProfileEdit = () => {
   const {
     fields: fieldsOther,
     append: appendOther,
-    remove: removeOthers,
+    remove: removeOther,
   } = useFieldArray({
     control,
     name: "others",
   });
 
-  // profile画像
   const [image, setImage] = useState(null);
-  // フリー投稿画像
   const [freeImage, setFreeImage] = useState(null);
 
-  const [profileImageURL, setProfileImageURL] = useState("");
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      try {
+        const response = await axios.get("http://localhost/api/profile/me", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        console.log(response.data);
+        setProfileData(response.data);
+        setValue("name", response.data.user.name);
+        setValue("birthday", response.data.user.birthday);
+        setValue("comment", response.data.user.comment);
+        setValue("title", response.data.freePosts[0].title);
+        setValue("description", response.data.freePosts[0].description);
+        setValue("otherName", response.data.otherData[0].newOtherName);
+        setNewOtherName(response.data.otherData[0].newOtherName || "その他");
+
+        console.log("Before removing:", fields, fieldsOther);
+        fields.length && fields.forEach((_, index) => remove(index));
+        fieldsOther.length &&
+          fieldsOther.forEach((_, index) => removeOther(index));
+        console.log("After removing:", fields, fieldsOther);
+
+        console.log(image);
+
+        // hobbiesデータのセット
+        console.log("Before appending hobbies:", fields);
+        if (response.data.hobbies && response.data.hobbies.length > 0) {
+          for (const hobby of response.data.hobbies) {
+            appendHobbies({ hobby: hobby.hobby });
+          }
+        }
+        console.log("After appending hobbies:", fields);
+
+        console.log("Before appending otherData:", fieldsOther);
+        if (response.data.otherData && response.data.otherData.length > 0) {
+          for (const other of response.data.otherData) {
+            appendOther({ name: other.name });
+          }
+        }
+        console.log("After appending otherData:", fieldsOther);
+        console.log(response.data.otherData[0]);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchProfileData();
+  }, [setValue, token, appendHobbies, appendOther]);
 
   const onImageChange = (event) => {
-    // eventで選択された画像ファイルをfile格納
     const file = event.target.files[0];
-    if (file) {
-      // 画像を表示するためにURLを生成して設定
-      const imageURL = URL.createObjectURL(file);
-      setProfileImageURL(imageURL);
-    }
-    console.log(file);
-    // imageをfileに更新する
     setImage(file);
+    // setImage("profile_image_path", response.data.image_url);
+
+    // setImage(file);
   };
+
   const onFreeImageChange = (event) => {
-    // eventで選択された画像ファイルをfile格納
     const file = event.target.files[0];
-    // imageをfileに更新する
     setFreeImage(file);
   };
 
-  const [newOtherName, setNewOtherName] = useState("その他");
-  const [token, setToken] = useState(Cookies.get("token") || null);
+  //   const [newOtherName, setNewOtherName] = useState("その他");
+  const [newOtherName, setNewOtherName] = useState(
+    profileData?.otherData[0]?.newOtherName || "その他"
+  );
 
-  // その他の項目名変更ボタンがクリックされたときのハンドラ
   const handleOtherNameChange = () => {
-    // フォームの新しい項目名を取得
-    const updatedOtherName = getValues("otherName"); // getValues を使用して値を取得
-    console.log(updatedOtherName);
+    const updatedOtherName = getValues("otherName");
     setNewOtherName(updatedOtherName);
-  };
-
-  const initializeDynamicFields = (data) => {
-    // 例: 趣味のデータが `hobbies` というキーで渡されると仮定
-    if (data.hobbies && Array.isArray(data.hobbies)) {
-      for (let hobby of data.hobbies) {
-        appendHobbies(hobby);
-      }
-    }
-
-    // その他の情報の初期化も同様に行う...
-    if (data.otherData && Array.isArray(data.otherData)) {
-      console.log("Others data:", data.otherData);
-
-      for (let other of data.otherData) {
-        appendOther({ name: other.name }); // appendOther を使用して「その他」のデータを動的に追加
-      }
-    }
   };
 
   const onSubmit = async (data) => {
     const formData = new FormData();
-    // appendを使ってobject形式で、formDataに入れていく
-    // ()の中のキー (birthday)はテーブルのカラム名と一致させる
-    // バリューのdataはhook-formのregisterで登録した入力値が入っている
     formData.append("name", data.name);
     formData.append("birthday", data.birthday);
     formData.append("comment", data.comment);
-
     data.hobbies.forEach((hobbyObj, index) => {
       formData.append(`hobbies[${index}][hobby]`, hobbyObj.hobby);
     });
-
     data.others.forEach((otherObj, index) => {
       formData.append(`others[${index}][name]`, otherObj.name);
     });
-
     formData.append("newOtherName", newOtherName);
-
     formData.append("title", data.title);
     formData.append("description", data.description);
-
     if (image) {
       formData.append("profile_image", image);
     }
@@ -110,67 +124,23 @@ const ProfileEdit = () => {
       formData.append("free_image", freeImage);
     }
 
+    console.log([...formData.entries()]);
+
     try {
-      const response = await axios.put(
-        "http://localhost/api/profile/me",
-        formData,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Content-Type": "multipart/form-data",
-          },
-        }
-      );
-      //   const receivedToken = response.data.token;
-      //   setToken(receivedToken);
-      //   Cookies.set("token", receivedToken, { expires: 7 });
+      await axios.post("http://localhost/api/profile/me", formData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "multipart/form-data",
+          "X-HTTP-Method-Override": "PUT", // PUTに置き換える記述を書く
+        },
+      });
     } catch (error) {
       console.log(error);
     }
   };
-  // 最初にプロフィールデータを取得する
-  //
-  // 最初にプロフィールデータを取得する
-  useEffect(() => {
-    const token = Cookies.get("token");
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("http://localhost/api/profile/me", {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        // ここで初めてデータをクリアする
-        removeHobbies();
-        removeOthers();
-
-        // その後すぐに新しいデータでフィールドを初期化
-        setProfileData(response.data);
-        setValue("name", response.data.user.name);
-        setValue("birthday", response.data.user.birthday);
-        setValue("comment", response.data.user.comment);
-        setValue("profileImage", response.data.user.profile_image_path);
-        setValue("title", response.data.freePosts[0].title);
-        setValue("description", response.data.freePosts[0].description);
-        setProfileImageURL(response.data.user.profile_image_path);
-
-        if (response.data.otherData[0].newOtherName) {
-          setNewOtherName(response.data.otherData[0].newOtherName);
-        }
-
-        initializeDynamicFields(response.data);
-
-        console.log(response.data);
-      } catch (error) {
-        console.log("データの取得に失敗");
-      }
-    };
-    fetchData();
-  }, []);
 
   return (
-    <>
+    <div>
       <div>ProfileForm</div>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
@@ -186,17 +156,22 @@ const ProfileEdit = () => {
           <textarea name="comment" {...register("comment")}></textarea>
         </div>
         <div>
-          <label>プロフィール画像:</label>
-          {/* 画像が選択されたらonImageChange関数が実行される */}
+          <label htmlFor="image">プロフィール画像</label>
           <input type="file" accept="image/*" onChange={onImageChange} />
-          {/* 選択画像の表示 */}
-          {image || (
+          {image ? (
             <img
-              // ファイルアップロードした画像から表示用のローカルURLを作成する
-              src={profileImageURL}
+              src={URL.createObjectURL(image)}
               alt="プロフィール画像"
-              style={{ width: "100px", height: "100px" }}
+              style={{ width: "200px", height: "200px" }}
             />
+          ) : (
+            profileData && (
+              <img
+                src={`http://localhost/${profileData.user.profile_image_path}`}
+                alt="プロフィール画像"
+                style={{ width: "200px", height: "200px" }}
+              />
+            )
           )}
         </div>
 
@@ -232,11 +207,6 @@ const ProfileEdit = () => {
           {fieldsOther &&
             fieldsOther.map((field, index) => (
               <div key={field.id}>
-                {/* <input
-                  name={`others[${index}].name`}
-                  {...register(`others[${index}].name`)}
-                  defaultValue={field.name}
-                /> */}
                 <input
                   name={`others[${index}].name`}
                   {...register(`others[${index}].name`)}
@@ -244,8 +214,8 @@ const ProfileEdit = () => {
                 />
               </div>
             ))}
-          {/* <button type="button" onClick={() => appendOther({ name: "" })}> */}
-          <button type="button" onClick={() => appendOther({ other: "" })}>
+          <button type="button" onClick={() => appendOther({ name: "" })}>
+            {/* <button type="button" onClick={() => appendOther({ other: "" })}> */}
             ＋
           </button>
         </div>
@@ -258,24 +228,33 @@ const ProfileEdit = () => {
           <input type="text" name="title" {...register("title")} />
           <p>説明</p>
           <textarea name="description" {...register("description")}></textarea>
-          <p>プロフィール画像:</p>
-          {/* 画像が選択されたらonImageChange関数が実行される */}
-          <input type="file" accept="image/*" onChange={onFreeImageChange} />
-          {/* 選択画像の表示 */}
-          {freeImage && (
-            <img
-              src={URL.createObjectURL(freeImage)}
-              alt="フリー投稿画像"
-              style={{ width: "100px", height: "100px" }}
-            />
-          )}
+          <p>フリー投稿画像:</p>
+          <div>
+            <label htmlFor="image">フリー画像</label>
+            <input type="file" accept="image/*" onChange={onFreeImageChange} />
+            {freeImage ? (
+              <img
+                src={URL.createObjectURL(freeImage)}
+                alt="プロフィール画像"
+                style={{ width: "200px", height: "200px" }}
+              />
+            ) : (
+              profileData && (
+                <img
+                  src={`http://localhost/${profileData.freePosts[0].image_path}`}
+                  alt="プロフィール画像"
+                  style={{ width: "200px", height: "200px" }}
+                />
+              )
+            )}
+          </div>
         </div>
 
         <div>
           <button type="submit">送信</button>
         </div>
       </form>
-    </>
+    </div>
   );
 };
 
