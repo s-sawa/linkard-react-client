5dc9cf3428ba59752c98e8c0bd3d220347e6f520
s-sawa <s.s.hrmeee21@gmail.com>
2023-09-30 22:15:09 +0900

    login registerCSS7割整備

--- src/App.jsx
+++ src/App.jsx
@@ -12,28 +12,42 @@ import Footer from "./components/Footer/Footer";
 import Modal from "react-modal";
 import Setting from "./pages/Setting/Setting";
 import "./App.css";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import PrivateRoute from "./components/PrivateRoute";
 
 Modal.setAppElement("#root");
 
 function App() {
-  const token = Cookies.get("token");
-
   return (
     <div className="container">
       <Routes>
-        <Route
-          path="/register"
-          element={token ? <Navigate to="/" /> : <Register />}
-        />
         <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Logout />} />
-        <Route path="/profile/setup" element={<ProfileForm />} />
-        <Route path="/" element={<ProfilePage />} />
-        {/* <Route path="/profile" element={<ProfilePage />} /> */}
-        <Route path="/profile/list" element={<ProfileList />} />
-        <Route path="/profile/edit" element={<ProfileEdit />} />
-        <Route path="/profile/:user_id/preview" element={<UserProfilePage />} />
-        <Route path="/setting" element={<Setting />} />
+        <Route path="/register" element={<Register />} />
+        {/* /login, /register以外のルートに対して以下がマッチする (path="/*" ワイルドカードのため) */}
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <Routes>
+                <Route path="/logout" element={<Logout />} />
+                <Route path="/profile/setup" element={<ProfileForm />} />
+                <Route path="/" element={<ProfilePage />} />
+                <Route path="/profile/list" element={<ProfileList />} />
+                <Route path="/profile/edit" element={<ProfileEdit />} />
+                {/* <Route
+                  path="/profile/:user_id/preview"
+                  element={<UserProfilePage />}
+                /> */}
+                <Route
+                  path="/profile/:user_id/preview"
+                  element={<ProfilePage />}
+                />
+                <Route path="/setting" element={<Setting />} />
+                <Route path="*" element={<NotFoundPage />} />
+              </Routes>
+            </PrivateRoute>
+          }
+        />
       </Routes>
       <Footer />
     </div>
